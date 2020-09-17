f5698e190b91ecb466c63168a3bb55e01bf3d03e
sale7268 <60412828+sale7268@users.noreply.github.com>
2020-09-17 16:54:34 -0700

    EditCourseActivity and DeleteCourseActivity added. fully working

---
+++ app/src/main/java/com/example/project1_gradetracker/EditCourseActivity.java
@@ -0,0 +1,108 @@
+package com.example.project1_gradetracker;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.project1_gradetracker.DB.Course;
+import com.example.project1_gradetracker.DB.CourseDAO;
+import com.example.project1_gradetracker.DB.User;
+
+import java.util.List;
+
+import static com.example.project1_gradetracker.LoginActivity.USER_NAME;
+import static com.example.project1_gradetracker.LoginActivity.database;
+
+public class EditCourseActivity extends AppCompatActivity {
+
+    private EditText editTitle, editID, editInstructor;
+    private EditText editStart, editEnd, editDescription;
+
+    private Button editButton;
+
+    List<Course> courseList;
+    public static CourseDAO courseDAO;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_course);
+
+        courseDAO = database.courseDAO();
+        courseList = courseDAO.getAllCourses();
+
+        editTitle = findViewById(R.id.etTitle);
+        editID = findViewById(R.id.etID);
+        editInstructor = findViewById(R.id.etInstructor);
+        editStart = findViewById(R.id.etStart);
+        editEnd = findViewById(R.id.etEnd);
+        editDescription = findViewById(R.id.etDescription);
+        editButton = findViewById(R.id.btnCreateC);
+
+        editButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent i = getIntent();
+                String user_name = i.getStringExtra(USER_NAME);
+                User user = null;
+
+                for(User u : database.userDAO().getAllUsers()) {
+                    if (u.getUsername().equals(user_name)) {
+                        user = u;
+                        break;
+                    }
+                }
+                if(user == null){
+                    Toast.makeText(EditCourseActivity.this, "no user found", Toast.LENGTH_SHORT).show();
+                }
+
+                if(editID.getText().toString().isEmpty()){
+                    editID.setError("ID field cannot be empty");
+                }
+                if(editTitle.getText().toString().isEmpty()){
+                    editTitle.setError("Title field cannot be empty");
+                }
+                if(editInstructor.getText().toString().isEmpty()){
+                    editInstructor.setError("Instructor field cannot be empty");
+                }
+
+                Course course;
+                int id = Integer.parseInt(editID.getText().toString());
+                String title = editTitle.getText().toString();
+                String instructor = editInstructor.getText().toString();
+                String desc = editDescription.getText().toString();
+                boolean checkcourse = false;
+
+                for(Course c: courseList){
+                    if(c.getCourseID() == id && c.getTitle().equals(title)){
+                        courseDAO.delete(c);
+                        course = new Course(id, title, instructor, desc);
+                        courseDAO.insert(course);
+                        checkcourse = true;
+                        Toast.makeText(EditCourseActivity.this, "Course: " + title + " Successfully edited", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                if(!checkcourse){
+                    Toast.makeText(EditCourseActivity.this, "Course: " + title + " Doesn't exist!", Toast.LENGTH_SHORT).show();
+                }
+
+                Intent intent = OverallGradeActivity.getIntent(getApplicationContext(), user_name);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public static Intent getIntent(Context context, String username){
+        Intent intent = new Intent(context, EditCourseActivity.class);
+        intent.putExtra(USER_NAME, username);
+
+        return intent;
+    }
+}
